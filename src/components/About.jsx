c53184c5d6e4e6f4bd1a1d8f079e048b300adc60
Diego-Calderon <diego.calderon@exos.mx>
2024-03-06 00:06:37 -0600

    fix font about cards

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -33,7 +33,7 @@ const ServiceCard = ({ index, title, icon, field }) => {
         className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
       >
         <img src={icon} alt={title} className="w-16 h-16 object-contain" />
-        <h3 className="text-taupe text-[18px] font-bold text-center">
+        <h3 className="text-taupe text-[18px] font-poppins text-center">
           {t(field)}
         </h3>
       </div>
